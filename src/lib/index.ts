655cd30a726b3036cdb7d64a8d3f97d0df7e4ac1
noxxxx <brooks2harry@gmail.com>
2025-03-27 13:57:29 +0800

    fix: 🐛 export constnat

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
 import Calendar from './calendar'
 export default Calendar
-export { ViewMode } from './calendar/constant'
+export { TimeConstraintsKeys, ViewMode } from './calendar/constant'
 export type { onChangeFn } from './calendar/type'
